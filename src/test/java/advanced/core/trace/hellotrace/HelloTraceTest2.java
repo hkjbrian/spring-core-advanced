489b928bb2f0254e8f1d6cc264fc3ec953147d2b
Han <hkjbrian@gmail.com>
2025-09-07 17:42:50 +0900

    test: HelloTraceV2 테스트 작성 - beginSync 동작확인

---
+++ src/test/java/advanced/core/trace/hellotrace/HelloTraceTest2.java
@@ -0,0 +1,25 @@
+package advanced.core.trace.hellotrace;
+
+import advanced.core.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class HelloTraceTest2 {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalArgumentException());
+        trace.exception(status1, new IllegalArgumentException());
+    }
+}
