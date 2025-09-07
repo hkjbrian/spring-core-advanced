880c356df83befd7eb4da7febdcb5f5d66467f29
Han <hkjbrian@gmail.com>
2025-09-07 17:51:26 +0900

    feat: 각각의 애플리케이션 메서드에서 TraceId를 파라미터로 전달받아서 로그 문맥을 이을 수 있도록 한다

---
+++ src/main/java/advanced/core/v2/OrderServiceV2.java
@@ -0,0 +1,28 @@
+package advanced.core.v2;
+
+import advanced.core.trace.TraceId;
+import advanced.core.trace.TraceStatus;
+import advanced.core.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId,"OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
