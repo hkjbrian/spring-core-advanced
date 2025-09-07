5dc16b8e0ec02b1d5b90afe0e1725d72956a0f24
Han <hkjbrian@gmail.com>
2025-09-07 17:16:18 +0900

    feat: service, repository 에 trace 적용

--- src/main/java/advanced/core/v1/v0/OrderRepositoryV1.java
+++ src/main/java/advanced/core/v1/v0/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package advanced.core.v1.v0;
 
+import advanced.core.trace.TraceStatus;
+import advanced.core.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,12 +9,25 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
     public void save(String itemId) {
-        // 저장 로직
-        if (itemId.equals("ex")) {
-            throw new IllegalArgumentException("예외 발생");
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalArgumentException("예외 발생");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
         }
-        sleep(1000);
     }
 
     private void sleep(int millis) {
