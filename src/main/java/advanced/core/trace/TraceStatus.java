65d2255f694c06a0c02fca0f036f46b7f497fe09
Han <hkjbrian@gmail.com>
2025-09-07 16:31:56 +0900

    feat: TraceStatus 생성 - 현재 트랜잭션의 시작 정보를 담는 객체, 요청을 종료할 때 활용한다

---
+++ src/main/java/advanced/core/trace/TraceStatus.java
@@ -0,0 +1,13 @@
+package advanced.core.trace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TraceStatus {
+
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+}
