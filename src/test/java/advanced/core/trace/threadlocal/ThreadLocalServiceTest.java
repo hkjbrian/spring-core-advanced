b9517401555afa1afefec8dcb9c0a285ab470d03
Han <hkjbrian@gmail.com>
2025-09-08 23:49:45 +0900

    feat: ThreadLocal을 활용한 서비스를 통해서 동시성 문제가 발생하지 않게 된 테스트 코드 작성 확인

---
+++ src/test/java/advanced/core/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package advanced.core.trace.threadlocal;
+
+import advanced.core.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+
+        sleep(3000);
+        log.info("main end");
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
