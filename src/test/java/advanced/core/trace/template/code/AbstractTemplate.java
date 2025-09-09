b604d1c3e4b4a0d55ca1c7dd417e9b4fd1879b34
Han <hkjbrian@gmail.com>
2025-09-09 22:24:26 +0900

    feat: 템플릿 메서드 패턴을 적용하여 공통 로직을 수행하는 코드를 담고 있는 추상 클래스 생성

---
+++ src/test/java/advanced/core/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,19 @@
+package advanced.core.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        long start = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        call();
+        //비즈니스 로직 종료
+        long end = System.currentTimeMillis();
+        long resultTime = end - start;
+        log.info("resultTime = {} ", resultTime);
+    }
+
+    protected abstract void call();
+}
