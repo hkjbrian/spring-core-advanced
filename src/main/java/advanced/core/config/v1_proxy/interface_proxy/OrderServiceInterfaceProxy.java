fa38f6c33fd0288693a4e09a1fdddf0931b8aa78
Han <hkjbrian@gmail.com>
2025-10-12 17:38:09 +0900

    feat: 인터페이스와 구현체가 분리되어 있는 상태에서의 프록시 적용

---
+++ src/main/java/advanced/core/config/v1_proxy/interface_proxy/OrderServiceInterfaceProxy.java
@@ -0,0 +1,27 @@
+package advanced.core.config.v1_proxy.interface_proxy;
+
+import advanced.core.sec5.app.v1.OrderServiceV1;
+import advanced.core.trace.TraceStatus;
+import advanced.core.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderServiceInterfaceProxy implements OrderServiceV1 {
+
+    private final OrderServiceV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.request()");
+            //target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
